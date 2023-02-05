e2871167cf6bd898541d0663cf02b44bab7446a0
yuta-2001 <85932615+yuta-2001@users.noreply.github.com>
2023-02-05 16:06:31 +0900

    花火を打ち上げる

--- components/header.js
+++ components/header.js
@@ -4,25 +4,32 @@ import Image from "next/image";
 
 export default function Header () {
 	return (
-		<header className="fixed w-full top-0 left-0">
+		<header className="fixed z-50 w-full top-0 left-0 font-serif">
 			<div className="container w-4/5 mx-auto flex items-center">
-				<Link href="/" className="w-40 h-40 mr-auto">
-					<Image src="/MINETECH.png" width={150} height={150} alt="logo" className="block w-full h-full" />
-				</Link>
+				<h1 className="mr-auto w-40 h-40">
+					<Link href="/" className="block w-full h-full duration-75 hover:opacity-70">
+						<Image src="/MINETECH.png" width={150} height={150} alt="logo" className="block w-full h-full" />
+					</Link>
+				</h1>
 				<nav className="text-lg">
 					<ul className="flex">
-						<li className="inline-block">
-							<Link href="/" className="inline-block p-2">
+						<li>
+							<Link href="/" className="inline-block p-2 text-white tracking-wide duration-75 hover:text-pink-500">
+								PROFILE
+							</Link>
+						</li>
+						<li className="ml-5">
+							<Link href="/" className="inline-block p-2 text-white tracking-wide duration-75 hover:text-pink-500">
 								WORKS
 							</Link>
 						</li>
-						<li className="ml-3">
-							<Link href="/" className="inline-block p-2">
+						<li className="ml-5">
+							<Link href="/" className="inline-block p-2 text-white tracking-wide duration-75 hover:text-pink-500">
 								SKILL
 							</Link>
 						</li>
-						<li className="ml-3">
-							<a href="https://www.yupro.tech/" target="_blank" rel="noopener" className="inline-block p-2">
+						<li className="ml-5">
+							<a href="https://www.yupro.tech/" target="_blank" rel="noopener" className="inline-block p-2 text-white tracking-wide duration-75 hover:text-pink-500">
 								Blog
 							</a>
 						</li>
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Layout from "../components/layout";
 import Link from 'next/link';
 import { client } from '../libs/client';
 import Image from 'next/image';
+import { Fireworks } from '@fireworks-js/react'
 
 //SSG
 export const getStaticProps = async() => {
@@ -19,27 +20,66 @@ export default function Home({ portfolio }) {
   console.log(portfolio);
   return (
     <Layout>
-      {/* <ul className={styles.list}>
-        {portfolio.map((portfolio) => (
-          <li key={portfolio.id}>
-            <Link href={`portfolio/${portfolio.id}`}>
-              <Image
-                src={portfolio.img.url}
-                width={portfolio.img.width}
-                height={portfolio.img.height}
-              />
-              <div>
-                <h3>
-                  {portfolio.name}
-                </h3>
-                <p>
-                  {portfolio.category.name ?? 'インターン'}
-                </p>
-              </div>
-            </Link>
-          </li>
-        ))}
-      </ul> */}
+      <div className="w-screen h-screen  text-white relative">
+        <h2 className="text-center absolute top-1/2 left-1/2  w-full -translate-y-2/4 -translate-x-2/4">
+          <span className="font-serif tracking-widest font-bold text-6xl mb-6 inline-block">Yuta Sugimine</span><br />
+          <span className="font-mono inlin-block">It is a self-introduction site of a university student enginner.</span>
+        </h2>
+      </div>
+      <Fireworks
+        options={{
+          hue: {
+            min: 0,
+            max: 209
+          },
+          acceleration: 1.00,
+          brightness: {
+            min: 4,
+            max: 95
+          },
+          decay: {
+            min: 0.013,
+            max: 0.033
+          },
+          delay: {
+            min: 24.52,
+            max: 55.00
+          },
+          explosion: 4,
+          flickering: 50.00,
+          intensity: 15.00,
+          friction: 0.96,
+          gravity: 1.52,
+          opacity: 0.1,
+          particles: 103,
+          traceLength: 5.70,
+          traceSpeed: 13,
+          rocketsPoint: {
+            min: 0,
+            max: 100
+          },
+          lineWidth: {
+            explosion: {
+              min: 0.83,
+              max: 5.65
+            },
+            trace: {
+              min: 0.10,
+              max: 4.84
+            }
+          },
+          lineStyle: "round"
+        }}
+        style={{
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          position: 'fixed',
+          background: '#000',
+          zIndex: -1
+        }}
+      />
     </Layout>
   )
 }
